7a1485b561450afd37ae44598ee1d2006ae5f1fb
wanghongfu0405 <869798794@qq.com>
2018-10-16 16:50:07 +0800

    Initial commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="6408fba3-daa4-44f7-9f21-b02415ae6eb4")
+public class DataBindingInfo {}
